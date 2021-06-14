c6e6ae2f94f10b041ebd7bcf35279bfdb3f4e909
princeyyadav <a3na.promachos@gmail.com>
2021-06-14 22:07:34 +0530

    dunders, numpy, pyplot

---
+++ Session 7_8/module2.py
@@ -0,0 +1,19 @@
+def factorial(n):
+    i = 2
+    ans = 1
+    while i<=n:
+        ans *= i
+        i += 1
+    return ans
+
+def func(s):
+    print(s)
+
+def func2(s):
+    print(s)
+
+x = 100
+
+class Dog:
+
+    pass
---
+++ Session 7_8/mymodule.py
@@ -0,0 +1,17 @@
+def factorial(n):
+    i = 2
+    ans = 1
+    while i<=n:
+        ans *= i
+        i += 1
+    return ans
+
+def func(s):
+    print(s)
+
+x = 100
+
+class Dog:
+
+    pass
+

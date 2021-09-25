837aea3f8aa370032e7a1d1c14c57988b80f3b35
princeyyadav <a3na.promachos@gmail.com>
2021-09-25 19:41:14 +0530

    add ReLU

--- DS/NN/activation.py
+++ DS/NN/activation.py
@@ -12,3 +12,17 @@ class Sigmoid:
         I = np.identity(X.shape[1])
         b = self.eval(X)*(1-self.eval(X)) # same shape as X
         return np.einsum('ij,mi->mij', I, b)
+
+class ReLU:
+
+    def __call__(self, X):
+        return self.eval(X)
+
+    def eval(self, X):
+        return X*(X>=0)
+
+    def grad_input(self, X):
+        I = np.identity(X.shape[1])
+        b = np.ones(X.shape) # same shape as X
+        b[X<0] = 0
+        return np.einsum('ij,mi->mij', I, b)
